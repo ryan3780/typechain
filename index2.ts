01161fa0c27b0fcfe4e31f943473836113dc16ac
ryan3780 <guemhong3780@gmail.com>
2019-03-07 07:12:38 +0900

    2

---
+++ index2.ts
@@ -0,0 +1,19 @@
+interface Human {
+    name: string,
+    gender: string,
+    age: number
+}
+
+const person = {
+    name: "ele",
+    gender: "male",
+    age: 28
+};
+
+const sayHi = (name: string, age: number, gender: string): string => {
+    //파라미터의 타입을 : 으로 지정이 가능하다. 또한 :string 으로 반환값의 타입을 정할 수 있다.
+    return `hello ${name}, you are ${age}, you are a ${gender}!`;
+};
+
+console.log(sayHi(person));
+export { };
